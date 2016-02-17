b80a34739a859c31668c1565149cdea11889362e
Sid Raval <sid@thoughtbot.com>
2016-02-16 19:45:16 -0500

    Initial commit

---
+++ src-bin/main.hs
@@ -0,0 +1,4 @@
+import qualified Reflex.FormValidation
+
+main :: IO ()
+main = Reflex.FormValidation.main
---
+++ src/Reflex/FormValidation.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE RecursiveDo, ScopedTypeVariables, FlexibleContexts, TypeFamilies, ConstraintKinds, TemplateHaskell #-}
+
+module Reflex.FormValidation where
+
+import Reflex
+import Reflex.Dom
+
+main :: IO ()
+main = undefined
